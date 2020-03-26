4f9bc1875c1bdfb4d3cbcec532c73237643fbb27
giselehandcom <gisele@handcom.com.br>
2020-03-26 17:39:02 -0300

    aula 2

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes')
 
 const app = express();
 
+app.use(cors());//em produção manda o origin que eh qual endereço pode acessar a aplicação
 app.use(express.json());
-
 app.use(routes);
-
 app.listen(3333);
 
